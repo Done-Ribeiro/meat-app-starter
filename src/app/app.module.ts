c5a204f4ebc7e890ea1de49db37d1d7f4517eb88
Done Ribeiro <www.done_@hotmail.com>
2021-08-24 18:16:36 -0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes'
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
@@ -18,26 +15,15 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { RatingComponent } from './shared/rating/rating.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { InputComponent } from './shared/input/input.component';
 
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
-
-
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -45,22 +31,15 @@ import { OrderService } from './order/order.service';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
-    OrderSumaryComponent,
-    RatingComponent
+    OrderSumaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,8 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
-import { AboutComponent } from './about/about.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
-import { OrderComponent } from './order/order.component'
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component'
 
 export const ROUTES: Routes = [
@@ -20,7 +18,7 @@ export const ROUTES: Routes = [
       { path: 'reviews', component: ReviewsComponent }
     ]
   },
-  { path: 'order', component: OrderComponent },
+  { path: 'order', loadChildren: './order/order.module#OrderModule' },
   { path: 'order-sumary', component: OrderSumaryComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', loadChildren: './about/about.module#AboutModule' }
 ]
